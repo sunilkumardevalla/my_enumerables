f685a477eb408ab58663c33b2e7abab955a06321
DsunilK <sunilkumardevalla@gmail.com>
2021-01-21 21:17:11 -0500

    add all any none methods

--- my_enumerables.rb
+++ my_enumerables.rb
@@ -16,6 +16,38 @@ module Enumerable
     array
   end
 
+  def my_all?
+    result = true
+    self.my_each { |i| break result = false unless yield(i) }
+    result
+  end
+
+  def my_any?
+    result = false
+    self.my_each { |i| break result = true if yield(i) }
+    result
+  end
+
+  def my_none?
+    result = true
+    self.my_each { |i| break = false if yield(i) }
+    result
+  end
+
+  def my_count(item)
+    count = 0
+    self.my_each { |i| count += 1 if item == i }
+    count
+  end
+
+  def 
+
+
+
+
+
+
+
 end
 
 
@@ -35,3 +67,24 @@ input.my_each_with_index { |i, index| puts index.to_s + ":" + i.to_s + ""}
 #my_select
 puts "my_select:"
 p input.my_select { |i| i < 5 }
+
+#my_all?
+puts "my_all?:"
+p input.my_all? { |i| i < 5 }
+
+#my_any?
+puts "my_any?:"
+p input.my_any? { |i| i < 5 }
+
+#my_none?
+puts "my_none?:"
+p input.my_none? { |i| i < 5 }
+
+#my_count
+puts "my_count:"
+p input.my_count(2)
+
+
+
+
+
