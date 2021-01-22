a33cd9226138595cc6bc7308a6644a2052d0e688
DsunilK <sunilkumardevalla@gmail.com>
2021-01-21 19:56:32 -0500

    add select method

--- my_enumerables.rb
+++ my_enumerables.rb
@@ -9,6 +9,13 @@ module Enumerable
     self.length.times { |i| yield(self[i], i) }
     self
   end
+
+  def my_select
+    array = []
+    self.my_each { |i| array << i if yield(i) }
+    array
+  end
+
 end
 
 
@@ -24,3 +31,7 @@ input.my_each { |i| p i.to_s + ""}
 #my_each_with_index
 puts "my_each_with_index:"
 input.my_each_with_index { |i, index| puts index.to_s + ":" + i.to_s + ""}
+
+#my_select
+puts "my_select:"
+p input.my_select { |i| i < 5 }
